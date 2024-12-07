c2afbe2c2c9e26632b8ee39bab71e6cf6d55bf4b
Julien Onorato <63990266+julien-onorato@users.noreply.github.com>
2024-12-07 14:39:56 +0100

    Update ScannerConfig.swift

--- Sources/ScannerKit/Models/ScannerConfig.swift
+++ Sources/ScannerKit/Models/ScannerConfig.swift
@@ -10,9 +10,15 @@ import AVFoundation
 import SwiftUI
 import CoreHaptics
 
-open struct ScannerConfig {
+public struct ScannerConfig {
     public let codeTypes: [AVMetadataObject.ObjectType]
     public let mode: ScanMode
     public let allowTorchToggle: Bool
 //    public let successHaptics: CHHapticEvent?
+
+    init(codeTypes: [AVMetadataObject.ObjectType], mode: ScanMode, allowTorchToggle: Bool) {
+        self.codeTypes = codeTypes
+        self.mode = mode
+        self.allowTorchToggle = allowTorchToggle
+    }
 }
